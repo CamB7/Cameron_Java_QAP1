e7f7f26797d61a2f664a0a7a9a170609e1ca7687
CamB7 <cameronboyer1999@gmail.com>
2025-09-12 22:01:53 -0230

    Got Project1 working

---
+++ src/Project1/Account.java
@@ -0,0 +1,48 @@
+package Project1;
+
+public class Account {
+	private String id;
+	private String name;
+	private int balance = 0;
+
+	// Constructor
+	public Account(String id, String name, int balance) {
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+
+	// Getters
+	public String getId() {return id;}
+	public String getName() {return name;}
+	public int getBalance() {return balance;}
+
+	public int credit(int amount){
+		balance += amount;
+		return balance;
+	}
+
+	public int debit(int amount) {
+		if (amount <= balance) {
+			balance -= amount;
+		} else {
+			System.out.println("Amount exceeded balance");
+		}
+		return balance;
+	}
+
+	public int transferTo(Account other, int amount) {
+		int before = this.balance;
+		this.debit(amount);
+		if (this.balance < before) {
+			other.credit(amount);
+		} else {
+			System.out.println("Amount exceeded balance");
+		}
+		return balance;
+	}
+
+	public String toString() {
+		return "Account[id=" + this.id + ", name=" + this.name + ", balance=" + this.balance + "]";
+	}
+}
---
+++ src/Project1/TestAccount.java
@@ -0,0 +1,19 @@
+package Project1;
+
+public class TestAccount {
+	public static void main(String[] args) {
+		Account Acc1 = new Account("001", "Cameron", 5000);
+		Account Acc2 = new Account("002", "Ainee", 4000);
+
+		System.out.println("Before transfer balances:");
+		System.out.println(Acc1.getName() + "'s balance: " + Acc1.getBalance());
+		System.out.println(Acc2.getName() + "'s balance: " + Acc2.getBalance());
+
+		Acc1.transferTo(Acc2, 1250);
+
+		System.out.println(" ");
+		System.out.println("After transfer balances:");
+		System.out.println(Acc1.getName() + "'s balance: " + Acc1.getBalance());
+		System.out.println(Acc2.getName() + "'s balance: " + Acc2.getBalance());
+	}
+}
